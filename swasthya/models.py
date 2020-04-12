0be818f8d2589b63c6473db72d02348f2b593bfd
rohanb1216 <rohan.biju@gmail.com>
2020-04-12 13:07:53 +0530

    formatting

--- swasthya/models.py
+++ swasthya/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import MinValueValidator
+# from django.core.validators import MinValueValidator
 
 # from django.utils.html import escape, mark_safe
 
@@ -9,34 +9,36 @@ class User(AbstractUser):
     is_doctor = models.BooleanField(default=False)
     is_patient = models.BooleanField(default=False)
 
-gender_list=(('M','M'),('F','F'),('Other','Other'))
+
+gender_list = (('M', 'M'), ('F', 'F'), ('Other', 'Other'))
+
 
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='patient_profile')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name='patient_profile')
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    contact= models.CharField(max_length=12)
-    email= models.EmailField()
-    gender = models.CharField(max_length=10,choices = gender_list, default = 'M')
-
-    
+    contact = models.CharField(max_length=12)
+    email = models.EmailField()
+    gender = models.CharField(max_length=10, choices=gender_list, default='M')
 
     def __str__(self):
         return self.user.username
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,related_name='doctor_profile')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name='doctor_profile')
     name = models.CharField(max_length=50)
     contact = models.CharField(max_length=12)
-    email= models.EmailField()
-    gender = models.CharField(max_length=10,choices = gender_list, default = 'M')
+    email = models.EmailField()
+    gender = models.CharField(max_length=10, choices=gender_list, default='M')
     qualification = models.CharField(max_length=250)
     specialisation = models.CharField(max_length=250)
-    
 
     def __str__(self):
         return self.user.username
+
 
 class Appointment(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
